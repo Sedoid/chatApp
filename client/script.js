073705697674ba1dd09f804247aad45d076aaa98
Sedoid <karlsedoide@gmail.com>
2019-12-02 10:12:09 +0100

    fixed the ui

--- client/script.js
+++ client/script.js
@@ -1,20 +1,71 @@
 // alert('Hello from the JS file')
+let userName,
+    form = document.querySelector('#message')
 
+console.log( Object.keys(localStorage))
+if(localStorage.getItem('userName')){
+    userName = localStorage.getItem('userName');
+
+     document.querySelector('#name').value = userName;
+
+}else{
+    console.log('found nothing like username in the storage')
+    userName = '';
+
+}
+    
 
 //$(document).ready(function(){
-    let socket = io();
+   let socket = io();
 
-    $('form').on('submit',function(){
+  form.addEventListener('input', ()=>{
+    socket.emit('typing', true)
+    })
+
+    $('form').on('submit',function(e){
+        e.preventDefault();
+      userName = $('#name').val();
+            localStorage.setItem('userName',userName);
+
+        // console.log('name:'+ userName);
         let text = $('#message').val();
-        text = 'Karlson says: '+ text;
-        
-        socket.emit('message',text);
-        $('#message').val('');
+        if(text!='')
+       {
+            text ={
+            senderName: userName,
+            message: text
+            };
+            
+            socket.emit('message', text)
+
+            // socket.emit('message',text);
+            // $('#message').val('');
+       }
 
         return false;
     });
 
-    socket.on('message',function(msg){
-        $('<li>').text(msg).appendTo('#history');
-    });
+    
+ socket.on('message',(data)=>{
+     console.log(data);
+          
+            let chat = document.createElement('li');
+    
+              chat.innerHTML =  `<div> <span> ${data.senderName} </span> <br /> <br />  ${data.message} </div>`;
+             let pattern = new RegExp(data.senderName);
+             userName = localStorage.getItem('userName');
+
+              if( userName == data.senderName)
+                   {
+                       console.log(data.senderName+ ' match '+ localStorage.getItem('userName'))
+                       chat.style.justifyContent = 'flex-end';
+                   } else{
+                       console.log('no match');
+                   }
+              document.querySelector('#history').appendChild(chat);
+        })
+    // socket.on('message',function(msg){
+    //     console.log( msg)
+    //     $('<li> ').text(msg.message).appendTo('#history');
+    // });
 //})
--- server.js
+++ server.js
@@ -1,20 +1,71 @@
-console.log('Hello from my node script!');
+console.log('Hello from my node script!buhahaha');
+
+
 
 let express = require('express'),
-    app = express(),
-    http = require('http'),
-    server = http.Server(app),
-    port = process.env.PORT || 8080;
+    socket = require('socket.io'),
+    http = require('http');
+    port = process.env.PORT || 4002,
+    fs = require('fs'),
+    path = require('path'),
+    url = require('url');
 
-app.use(express.static('client'));
+    app = express();
 
-let io = require('socket.io')(server);
+    let server = http.createServer(app)
+    // app.get('/',(req,res) =>{
+    // res.sendFile(path.join(__dirname,'client','index.html'));
+    //     // fs.readFile(path.join(__dirname,'./client/index.html'),(error,data)=>{
+    //     //   //  res.writeHead(200,{'Content-Type': 'text/html'});
+    //     //  //    res.write(data)
+          
+  
+    //     //  })
+    // })
 
-io.on('connection',function(socket){
-    socket.on('message',function(msg){
-        io.emit('message',msg);
-    });
-});
+   app.use(express.static(path.join(__dirname,'client')));
+
+    let io = socket(server);
+    let index = 0;
+        io.on('connection',function(socket){
+            //console.log(socket.id);
+            //console.log(++index +' user connected ');  
+
+            socket.on('message',function(data){
+                 console.log('message is being sent');
+                io.emit('message',data)
+               
+            })
+
+            socket.on('typing',function(state){
+                // io.broadcast.emit('typing',state)
+            })
+
+ })
+       
+
+        
+
+
+    server.listen(port,(error) => console.log('server up and running'));
+
+
+{
+ // let express = require('express'),
+//     app = express(),
+//     http = require('http'),
+//     server = http.Server(app),
+//     port = process.env.PORT || 8080;
+
+// app.use(express.static('client'));
+
+// let io = require('socket.io')(server);
+
+// io.on('connection',function(socket){
+//     socket.on('message',function(msg){
+//         io.emit('message',msg);
+//     });
+// });
 
 
 
@@ -26,6 +77,7 @@ io.on('connection',function(socket){
 
 
 
-server.listen(port,function(){
-    console.log('Chat server running');
-})
+// server.listen(port,function(){
+//     console.log('Chat server running');
+// })   
+}
